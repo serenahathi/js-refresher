909823b3fb09f8f6d486c29912b72d670174c741
“Serena <serenahathi@hotmail.com>
2018-05-23 15:17:23 +0200

    :construction: Store buttons clicked in variables

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 $(document).ready(function() {
 
-    let num1 = "";
-    let num2 = "";
-    let operator = "";
-    let total = "";
+    let firstNumber = '';
+    let secondNumber = '';
+    let calcOperation = '';
+    let total = '';
 
     $('button').on('click', function(e) {
-        console.log('e', e.target.innerHTML);
+        let button = e.target.innerHTML;
+        if (button >= '0' && button <= 9) {
+            handleNumber(button);
+            // console.log('number')
+        } else {
+            handleOperator(button);
+            // console.log('operator')
+        }
+        // console.log('e', e.target.innerHTML);
     });
+
+    function handleNumber(number) {
+        if (firstNumber === '') {
+            firstNumber = number;
+        } else {
+            secondNumber = number;
+        }
+    }
+    
+    function handleOperator(operator) {
+        calcOperation = operator
+    }
+
 
 });
