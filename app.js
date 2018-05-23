bb4a0144d2f4be5780c34040d8a119b75c107c91
“Serena <serenahathi@hotmail.com>
2018-05-23 14:57:18 +0200

    :construction: Display button clicked

---
+++ app.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    let num1 = "";
+    let num2 = "";
+    let operator = "";
+    let total = "";
+
+    $('button').on('click', function(e) {
+        console.log('e', e.target.innerHTML);
+    });
+
+});
