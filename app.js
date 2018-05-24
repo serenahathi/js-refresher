b805ff2a113831602ee3df8b72d4d384f9e8e038
“Serena <serenahathi@hotmail.com>
2018-05-24 15:35:46 +0200

    :sparkles: Handles double digits and decimals

--- app.js
+++ app.js
@@ -4,67 +4,71 @@ $(document).ready(function() {
     let secondNumber = '';
     let calcOperation = '';
     let total = '';
-
-    $('button').on('click', function(e) {
+  
+      $(".numbers button").on("click", function(e) {
         let button = e.target.innerHTML;
-        console.log(button)
-        if (button >= '0' && button <= 9) {
-            handleNumber(button);
-            console.log('number')
+        if ($(".active").length == 0) {
+          handleFirstNumber(button);
         } else {
-            handleOperator(button);
-            console.log('operator')
+          handleSecondNumber(button);
         }
-        console.log('e', e.target.innerHTML);
-    });
-
-    function handleNumber(number) {
-        if (firstNumber === '') {
-            firstNumber = number;
-        } else {
-            secondNumber = number;
+      });
+  
+      $(".operators button").on("click", function(e) {
+        let operator = e.target;
+        let button = e.target.innerHTML;
+        if (button !== "=") {
+          $(this).addClass("active");
+        } else if (button == "=") {
+          handleTotal();
         }
-        displayButton(number)
-    }
-
-    function handleOperator(operator) {
-        if (calcOperation === '') {
-            calcOperation = operator
-        } else {
-            handleTotal();
-            calcOperation = operator;
-        }
-    }
-
-    function handleTotal() {
+      });
+  
+      function handleFirstNumber(number) {
+        firstNumber = firstNumber + number;
+        displayButton(firstNumber)
+      };
+  
+      function handleSecondNumber(number) {
+        secondNumber = secondNumber + number;
+        displayButton(secondNumber)
+      };
+  
+      function handleTotal() {
+      calcOperation = $(".active").html();
         switch (calcOperation) {
-            case '+':
-                total = +firstNumber + +secondNumber;
-                displayButton(total);
-                break;
-            case '-':
-                total = +firstNumber - +secondNumber;
-                displayButton(total);
-                break;
-            case '/':
-                total = +firstNumber / +secondNumber;
-                displayButton(total);
-                break;
-            case '*':
-                total = +firstNumber * +secondNumber;
-                displayButton(total);
-                break;
-        }
-        updateVariables();
-    }   
-
-    function displayButton(button) {
+          case '+':
+            total = +firstNumber + +secondNumber;
+            total = total.toFixed(2);
+            displayButton(total);
+            break;
+          case '-':
+            total = +firstNumber - +secondNumber;
+            total = total.toFixed(2);
+            displayButton(total);
+            break;
+          case '/':
+            total = +firstNumber / +secondNumber;
+            total = total.toFixed(2);
+            displayButton(total);
+            break;
+          case '*':
+            total = +firstNumber * +secondNumber;
+            total = total.toFixed(2);
+            displayButton(total);
+            break;
+          }
+          updateVariables();
+      };
+  
+      function displayButton(button) {
         $('.calculator-result-input').text(button);
-    }
-
-    function updateVariables() {
-        firstNumber = total;
-        secondNumber = '';
-    }
-
-});
+      };
+  
+      function updateVariables() {
+          firstNumber = total;
+          secondNumber = '';
+          $(".active").removeClass()
+      };
+  });
+  
